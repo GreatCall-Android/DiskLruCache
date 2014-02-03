795ca8de65933c4bc847032efaaf08e5f2a1da1f
Christopher Van Kirk <Christopher.VanKirk@greatcall.com>
2014-02-03 13:47:18 -0800

    Minor cosmetic change.

--- src/test/java/com/jakewharton/disklrucache/StrictLineReaderTest.java
+++ src/test/java/com/jakewharton/disklrucache/StrictLineReaderTest.java
@@ -28,8 +28,7 @@ public class StrictLineReaderTest {
   @Test public void lineReaderConsistencyWithReadAsciiLine() {
     try {
       // Testing with LineReader buffer capacity 32 to check some corner cases.
-      StrictLineReader lineReader =
-          new StrictLineReader(createTestInputStream(), 32, Util.US_ASCII);
+      StrictLineReader lineReader = new StrictLineReader(createTestInputStream(), 32, Util.US_ASCII);
       InputStream refStream = createTestInputStream();
       while (true) {
         try {
